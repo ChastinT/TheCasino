ff424201467ce52f4a5f7b8ebbef9ea3da9c2e43
Brent Oliver <brentoliver@Brents-MBP.fios-router.home>
2020-07-08 15:57:10 -0400

    Updated and Tested Card Class

--- src/main/java/com/codedifferently/casino/Utilities/Card.java
+++ src/main/java/com/codedifferently/casino/Utilities/Card.java
@@ -1,3 +1,32 @@
+package com.codedifferently.casino.Utilities;
+
 public class Card {
-  
+
+    public enum Suit {HEARTS, DIAMONDS, SPADES, CLUBS}; // Suit can only be of these 4
+    public enum Value {ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING}; // Value can only be of these
+
+    private Suit suit;
+    private Value value;
+
+    // Make a card with specified suit and value
+    public Card(Suit aSuit, Value aValue) {
+        suit = aSuit;
+        value = aValue;
+    }
+
+    // Returns the suit of the card
+    public Suit getSuit() {
+        return suit;
+    }
+
+    // Returns the value of the card
+    public Value getValue() {
+        return value;
+    }
+
+    // Returns state of card (suit and value)
+    public String toString() {
+        return value + " OF " + suit;
+    }
+
 }
---
+++ src/test/java/com/codedifferently/casino/CardTest.java
@@ -0,0 +1,72 @@
+package com.codedifferently.casino;
+
+import com.codedifferently.casino.Utilities.Card;
+import com.codedifferently.casino.Utilities.Card.Value;
+import com.codedifferently.casino.Utilities.Card.Suit;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CardTest
+{
+
+    @Test
+    public void CardConstructorTest()
+    {
+        // Given
+        Suit cardSuit = Suit.HEARTS;
+        Value cardVal = Value.THREE;
+
+        // When
+        Card card = new Card(cardSuit, cardVal);
+        Suit actualSuit = card.getSuit();
+        Value actualVal = card.getValue();
+
+        // Then
+        Assert.assertEquals(cardSuit, actualSuit);
+        Assert.assertEquals(cardVal, actualVal);
+    }
+
+    Card card;
+    @Before
+    public void initialize() {
+        card = new Card(Suit.HEARTS, Value.THREE);
+    }
+
+    @Test
+    public void getSuitTest() {
+        // Given
+        Suit expectedSuit = Suit.HEARTS;
+
+        // When
+        Suit actualSuit = card.getSuit();
+
+        // Then
+        Assert.assertEquals(expectedSuit, actualSuit);
+    }
+
+    @Test
+    public void getValueTest() {
+        // Given
+        Value expectedVal = Value.THREE;
+
+        // When
+        Value actualVal = card.getValue();
+
+        // Then
+        Assert.assertEquals(expectedVal, actualVal);
+    }
+
+    @Test
+    public void toStringTest() {
+        // Given
+        String expected = "THREE OF HEARTS";
+
+        // When
+        String actual = card.toString();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+}
